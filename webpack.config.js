73a80e69e0cca37e19c86751d859e89bd5541c10
Joshua Stoutenburg <jehoshua02@gmail.com>
2015-06-10 12:13:15 -0600

    Update index.html to Pull In Compiled Application Script

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, './build'),
+    path: path.resolve(__dirname, './web/build'),
     filename: 'app.js'
   }
 };
